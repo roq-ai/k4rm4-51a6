180c653f501b2140ecc60b24cc2ee5c984a123b0
roq-ai <>
2023-07-02 19:09:44 +0000

    Initial commit

---
+++ src/validationSchema/tasks/index.ts
@@ -0,0 +1,8 @@
+import * as yup from 'yup';
+
+export const taskValidationSchema = yup.object().shape({
+  name: yup.string().required(),
+  status: yup.string().required(),
+  assigned_to: yup.string().nullable(),
+  company_id: yup.string().nullable(),
+});
